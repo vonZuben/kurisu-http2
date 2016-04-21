9288b105998c77fd7a90d9fff77959c17813bf99
Chris von Zuben <chris.vonzuben@gmail.com>
2016-04-20 20:04:33 -0400

    More testing, reading frame metadata

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ extern crate openssl;
 
 use openssl::ssl::*;
 
-use std::net::{TcpStream, TcpListener};
+use std::net::{TcpListener};
 use std::thread;
 use std::io::{Read, Write};
-use std::fs::File;
+//use std::fs::File;
 use std::str;
 use std::slice;
 //use std::sync::{Once, ONCE_INIT};
 //use std::cell::Cell;
+use std::fmt::Debug;
 
-//use std::mem;
+use std::mem;
 
 //#[path = "ssl.rs"]
 //mod ssl;
@@ -77,7 +78,37 @@ use std::slice;
 //    }
 //}
 
-fn handle_client(mut stream: SslStream<TcpStream>) {
+#[derive(Debug)]
+struct Frame<'a> {
+    length: u32,
+    f_type: u8,
+    f_flags: u8,
+    s_identifier: u32,
+    payload: &'a[u8],
+}
+
+impl<'a> Frame<'a> {
+    fn new(buf: &'a[u8]) -> Self {
+        Frame {
+            length: u32::from_le( unsafe { mem::transmute([ buf[2], buf[1], buf[0], 0u8 ]) } ),
+            f_type: buf[3],
+            f_flags: buf[4],
+            s_identifier: u32::from_le( unsafe { mem::transmute([ buf[8], buf[7], buf[6], buf[5] & 0x7F ]) } ),
+            payload: &buf[9..],
+        }
+    }
+}
+
+// bad function that is not acctualy safe to call
+fn print_hex(buf: &Vec<u8>, n: usize) {
+    let b: &[u8] = unsafe { slice::from_raw_parts(buf.as_ptr(), n) };
+    for i in 0..n {
+        print!("{:02X}:", b[i])
+    }
+    println!("\n");
+}
+
+fn handle_client<T: Read + Write + Debug>(mut stream: T) {
     //let mut buf: Vec<u8> = Vec::with_capacity(512);
     //let mut buf = vec![0;512];
     //let mut buf: Vec<u8> = Vec::with_capacity(512);
@@ -98,9 +129,11 @@ fn handle_client(mut stream: SslStream<TcpStream>) {
     match err {
         Ok(n) => {
             println!("ok: {}", n);
-            let req: &str = str::from_utf8(&buf[..n]).unwrap();
+            let req: &str = unsafe { str::from_utf8_unchecked(&buf[..n]) };
 
-            println!("{:?}", buf); // this is so hacky i live it
+            print_hex(&buf, n);
+
+            //println!("{:?}", buf); // this is so hacky i live it
             //let req: &str = unsafe { mem::transmute((&buf2, n)) };
             //let req: &str = str::from_utf8(&buf2).unwrap();
             //let req: &str = unsafe { str::from_utf8_unchecked(&buf2) };
@@ -111,22 +144,47 @@ fn handle_client(mut stream: SslStream<TcpStream>) {
 
     //let req = String::from_utf8(buf2).unwrap();
 
+    //stream.write(b"PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n").unwrap();
+    //stream.write(b"HELLO World").expect("couldn't write hw");
+
+    loop {
+        let err = stream.read(&mut buf);
+
+        println!("NEXT");
+
+        match err {
+            Ok(n) => {
+                if n == 0 { break; }
+                println!("ok: {}", n);
+                print_hex(&buf, n);
+                let f = Frame::new(&buf[..n]);
+                println!("{:?}", f);
+
+                if f.length > 200 {
+                    let b = [0u8, 0, 0, 4, 1, 0, 0, 0, 0];
+                    stream.write(&b).unwrap();
+                    println!("{}", unsafe { str::from_utf8_unchecked(f.payload) } );
+                }
+            },
+            Err(e) => {println!("err: {}", e); break;},
+        }
+    }
 
     //print!("HTTP/1.1 200 OK\nServer: Apache/2.2.14 (Win32)\nContent-Type: text/html\nConnection: Closed\n\nTEST\n\n");
 
-    if let Ok(f) = File::open("html/index.html") {
-        println!("{:?}", f);
-        stream.write(b"HTTP/1.1 200 OK\nServer: whiteToken TESTING\nContent-Type: text/html\nConnection: Closed\n\n").unwrap();
-        for b in f.bytes() {
-            //println!("{}", b.unwrap());
-            //stream.write(unsafe { mem::transmute((&b.unwrap(), 1u64)) } );
-            stream.write( unsafe { slice::from_raw_parts(&b.unwrap(), 1) } ).unwrap();
-            //write!(stream, "{:x}", b.unwrap());
-        }
-    }
-    else {
-        stream.write(b"HTTP/1.1 401\n\n").unwrap();
-    }
+    //if let Ok(f) = File::open("html/index.html") {
+    //    println!("{:?}", f);
+    //    //stream.write(b"HTTP/1.1 200 OK\nServer: whiteToken TESTING\nContent-Type: text/html\nConnection: Closed\n\n").unwrap();
+    //    for b in f.bytes() {
+    //        //println!("{}", b.unwrap());
+    //        //stream.write(unsafe { mem::transmute((&b.unwrap(), 1u64)) } );
+    //        stream.write( unsafe { slice::from_raw_parts(&b.unwrap(), 1) } ).unwrap();
+    //        //write!(stream, "{:x}", b.unwrap());
+    //    }
+    //}
+    //else {
+    //    stream.write(b"HTTP/1.1 401\n\n").unwrap();
+    //}
     //let mut s = String::new();
     //try!(f.read_to_string(&mut s));
     //assert_eq!(s, "Hello, world!");
@@ -139,42 +197,6 @@ fn handle_client(mut stream: SslStream<TcpStream>) {
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-
-//    let mut buf: Vec<u8> = Vec::with_capacity(512);
-//
-//    let (b, s): (&[u8; 512], usize) = unsafe{ mem::transmute(buf.as_slice()) };
-//
-//    println!("size: {}", s);
-//
-//    let mut buf2: [u8; 512] = unsafe { mem::uninitialized() };
-//
-//    for t in 0..512 {
-//        print!("{:x}", buf2[t]);
-//    }
-//    println!("flushed");
-
-//    let x = Cell::new(1);
-//    let y = &x;
-//    let z = &x;
-//    x.set(2);
-//    y.set(3);
-//    z.set(4);
-//    println!("{}", x.get());
-
-    //let mut buf: Box<[u8; 512]> = unsafe { mem::uninitialized() };
-
-    //unsafe {
-    //    SSL_library_init();
-    //    SSL_load_error_strings();
-    //}
-
-    //let mut ctx = Ctx::new();
-
-    //ctx.config();
-
-    //for i in 0..buf.len() {
-    //    println!("{}", buf[i]);
-    //}
 
     let mut ctx: SslContext = SslContext::new(SslMethod::Tlsv1_2).unwrap();
 
@@ -205,12 +227,20 @@ fn main() {
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
+            Ok(mut stream) => {
                 let tls = Ssl::new(&ctx).unwrap();
                 thread::spawn(move|| {
-                    // connection succeeded
-                    let tls_stream = SslStream::accept(tls, stream).unwrap();
-                    handle_client(tls_stream)
+                    if let Ok(tls_stream) = SslStream::accept(tls, &stream) {
+                        handle_client(tls_stream);
+                        return;
+                    }
+
+                    println!("NOT TLS");
+                    //let mut b: [u8;512] = unsafe { mem::uninitialized() };
+                    //let n = stream.read(&mut b).unwrap();
+                    stream.write_all(b"HTTP/1.1 301 Moved Permanently\r\nLocation: https://127.0.0.1:8080\r\n\r\n").unwrap();
+                    //println!("size: {}: {}", n, str::from_utf8(&b[..n]).unwrap());
+                    stream.shutdown(std::net::Shutdown::Both).unwrap();
                 });
             }
             Err(_) => { /* connection failed */ }
