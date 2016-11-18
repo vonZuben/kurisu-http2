09641cc739246a5bd63b9aecd25f8e0ccfb46de1
Chris von Zuben <chris.vonzuben@gmail.com>
2016-11-18 17:40:09 -0500

    Remove some frame type documentation
    Should just refer to RFC for full docs

--- src/frame/frame_types.rs
+++ src/frame/frame_types.rs
@@ -54,25 +54,9 @@ impl_debug_print!( GenericFrame );
 
 impl_buf_frame!( HeadersFrame );
 
-/// ===============================
-/// HEADER FLAGS
-/// ===============================
-///
-/// END_STREAM (0x1):
-/// When set, bit 0 indicates that the header block (Section 4.3) is the last that the endpoint will send for the identified stream.
-///
-/// A HEADERS frame carries the END_STREAM flag that signals the end of a stream. However, a HEADERS frame with the END_STREAM flag set can be followed by CONTINUATION frames on the same stream. Logically, the CONTINUATION frames are part of the HEADERS frame.
-///
-/// END_HEADERS (0x4):
-/// When set, bit 2 indicates that this frame contains an entire header block (Section 4.3) and is not followed by any CONTINUATION frames.
-///
-/// A HEADERS frame without the END_HEADERS flag set MUST be followed by a CONTINUATION frame for the same stream. A receiver MUST treat the receipt of any other type of frame or a frame on a different stream as a connection error (Section 5.4.1) of type PROTOCOL_ERROR.
-///
-/// PADDED (0x8):
-/// When set, bit 3 indicates that the Pad Length field and any padding that it describes are present.
-///
-/// PRIORITY (0x20):
-/// When set, bit 5 indicates that the Exclusive Flag (E), Stream Dependency, and Weight fields are present; see Section 5.3.
+// ================================================
+// the major header types are defined as follows
+// ================================================
 
 const END_STREAM : u8 = 0x1;
 const END_HEADERS : u8 = 0x4;
@@ -96,20 +80,6 @@ const PRIORITY : u8 = 0x20;
 ///  +---------------------------------------------------------------+
 /// Figure 7: HEADERS Frame Payload
 ///
-/// The HEADERS frame payload has the following fields:
-///
-/// Pad Length:
-/// An 8-bit field containing the length of the frame padding in units of octets. This field is only present if the PADDED flag is set.
-/// E:
-/// A single-bit flag indicating that the stream dependency is exclusive (see Section 5.3). This field is only present if the PRIORITY flag is set.
-/// Stream Dependency:
-/// A 31-bit stream identifier for the stream that this stream depends on (see Section 5.3). This field is only present if the PRIORITY flag is set.
-/// Weight:
-/// An unsigned 8-bit integer representing a priority weight for the stream (see Section 5.3). Add one to the value to obtain a weight between 1 and 256. This field is only present if the PRIORITY flag is set.
-/// Header Block Fragment:
-/// A header block fragment (Section 4.3).
-/// Padding:
-/// Padding octets.
 
 // helper function to get 32bit numbers from the big endian input stream
 unsafe fn getu32_from_be(buf: &[u8]) -> u32 {
